3fe5ca63a38f9b37c59f2f2f4244487a5497c952
Gonzalo Cobos Daract <goncobos@gmail.com>
2022-12-28 20:11:55 -0300

    Creada App, con vistas, modelos, controlador funcionando

---
+++ app/helpers/proudcts_helper.rb
@@ -0,0 +1,2 @@
+module ProudctsHelper
+end
---
+++ test/controllers/proudcts_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ProudctsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
